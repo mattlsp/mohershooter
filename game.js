404d58de508d37ebc186e3cdac4df79afeba6da1
mkowalczyk85 <maciej.kowalczyk@pentacomp.pl>
2019-12-26 04:17:32 +0100

    responsywnosc

--- game.js
+++ game.js
@@ -1,11 +1,17 @@
-
 var config = {
+	type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'phaser-example',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1280,
-        height: 720,
-        backgroundColor: 0x000000,
-		scene: [Scene1, Scene2]
+        height: 720
+    },
+    backgroundColor: 0x000000,
+	scene: [Scene1, Scene2]
 		
 }
+
 var game = new Phaser.Game(config);
 
-
+var babkaOdpulona = "false";
--- scene2.js
+++ scene2.js
@@ -10,8 +10,8 @@ class Scene2 extends Phaser.Scene {
         this.background = this.add.image(0, 0, "background");
         this.background.setOrigin(0, 0);
 		
-        this.add.text(20, 700, "MoherShooter v0.2 alpha. USTRZEL KONFITURE");
-
+        this.add.text(20, 700, "MoherShOOter v0.2 alpha. O D P U L  M O H E R A !!");
+		
         // BABKA 1
         this.babka1 = this.add.sprite(1170, 490, "b1");
 
@@ -36,7 +36,7 @@ class Scene2 extends Phaser.Scene {
         });
         this.babka1.play("b1_anim");
         this.babka1.setInteractive();
-
+		
         // BABKA 2
         this.babka2 = this.add.sprite(1171, 480, "b2");
 
@@ -131,20 +131,15 @@ class Scene2 extends Phaser.Scene {
 	killBot(pointer, gameObject) {
 		
 		var key = gameObject.texture.key;
-		console.log(key);
 		gameObject.setTexture(key + '_kill');
 		gameObject.play(key + '_kill_anim');
 		this.decapitationSound.play();
-		console.log('ZATLUKLES MOHERA!');
+		babkaOdpulona = true;
+		console.log("bot odpulony: " + babkaOdpulona);
 	
     }
-	
-	startMusic(pointer, gameObject){
-		console.log("kliknales w MUSIC");
-	}
-	
-	
-	// SZTYWNA SCIEZKA RUCHU MOHERA1
+		
+	// SZTYWNA SCIEZKA RUCHU BABKI1
 	path1(babka, speed){
 		
 		babka.x -= 2.5*speed; 
@@ -165,7 +160,7 @@ class Scene2 extends Phaser.Scene {
 		}
 				
 	}
-	// SZTYWNA SCIEZKA RUCHU MOHERA2
+	// SZTYWNA SCIEZKA RUCHU BABKI2
 	path2(babka, speed){
 		
 		babka.x -= 2*speed; 
@@ -204,7 +199,6 @@ class Scene2 extends Phaser.Scene {
             this.babka1.x = 1170;
             this.babka1.y = 490;
 
-			
 		}
 		
 		// UKRYWANIE BABKI2 NA LEWO OD 374PX [KLATKA KOSAKA]
@@ -216,6 +210,7 @@ class Scene2 extends Phaser.Scene {
 			
 		} else {
 			
+			// RUCH BABEK PO SCIEZKACH
 			if (true) {
 				
 				this.path1(this.babka2, 1);
@@ -224,6 +219,15 @@ class Scene2 extends Phaser.Scene {
 			}
 			
 		}
+		
+		//if (babkaOdpulona == true) {
+			
+			//this.babka1.setTexture(this.b1);
+			//this.babka1.x = 1170;
+            //this.babka1.y = 490;
+			//this.path2(this.babka1, 2);
+		
+		//}
 		
 	}
 	
