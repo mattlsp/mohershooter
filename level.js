b8b09b55e35d2cacbe7f14737840eba223823e28
mkowalczyk85 <maciej.kowalczyk@pentacomp.pl>
2020-12-31 00:32:08 +0100

    added bot respawn and speed acceleration

--- level.js
+++ level.js
@@ -7,6 +7,7 @@ class LevelOne extends Phaser.Scene {
     create() {
 
 		this.score = 0;
+		this.acceleration = 1;
 		
         // SETUP TŁA
         this.background = this.add.image(0, 0, "background");
@@ -14,7 +15,7 @@ class LevelOne extends Phaser.Scene {
 		
 		this.scoreDisplay = this.add.text(16,16, 'Score: 0', { font: '32px Consolas', fill: '#fff' });
 		
-        // BABKA 1
+				// BABKA 1
         this.babka1 = this.add.sprite(1170, 490, "b1");
 
         this.babka1.setOrigin(0, 0);
@@ -33,7 +34,7 @@ class LevelOne extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers("b1_kill"),
             frameRate: 10,
             repeat: 0,
-            hideOnComplete: true
+            hideOnComplete: false
 
         });
         this.babka1.play("b1_anim");
@@ -57,12 +58,12 @@ class LevelOne extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers("b2_kill"),
             frameRate: 10,
             repeat: 0,
-            hideOnComplete: true
+            hideOnComplete: false
 
         });
         this.babka2.play("b2_anim");
         this.babka2.setInteractive();
-
+		
         // WYSWIETLANIE SPRITE'U SAMOCHODU
         this.furka = this.add.image(387, 515, "car");
         this.furka.setOrigin(0, 0);
@@ -75,51 +76,56 @@ class LevelOne extends Phaser.Scene {
         this.smietnik = this.add.image(550, 430, "bin");
         this.smietnik.setOrigin(0, 0);
 
+
+
         // EVENT ODPULANIA MOHERA
-        let odpulony = false;
-        if (odpulony == false) {
-            this.input.on('gameobjectdown', this.killBot, this)
-			
-        }
-        else {
-            console.log("bot juz odpulony")
-			
-        }
+        this.input.on('gameobjectdown', this.killBot, this)
+		
+		let babki = [this.babka1,this.babka2];
+		babki.forEach( (babka) => {
+	    	babka.defaultSprite = babka.texture.key;
+	    	babka.defaultAnimation = babka.anims.currentAnim.key;
+	    });
         
 		// SFX ODPULANIA MOHERA
 		this.decapitationSound = this.sound.add("sfx_babka_decapitated");
 
 	}
 	
-	// LEPSZA METODA ODPULANIA MOHERA
+	// METODA ODPULANIA MOHERA
 	killBot(pointer, gameObject) {
 		
 		var key = gameObject.texture.key;
 		gameObject.setTexture(key + '_kill');
 		gameObject.play(key + '_kill_anim');
+		console.log(key);
+		gameObject.once('animationcomplete', () => {
+			gameObject.x = 1170;
+            gameObject.y = 490;
+			gameObject.play(key + '_anim');
+		});
 		this.decapitationSound.play();
 		this.score+=10;
 		this.scoreDisplay.setText('Score: '+this.score);
-		//babkaOdpulona = true;
-		//console.log("bot odpulony: " + babkaOdpulona);
-	
+		this.acceleration = this.acceleration + 0.1;
+		
     }
 		
 	// SZTYWNA SCIEZKA RUCHU BABKI1
 	path1(babka, speed){
 		
-		babka.x -= 2.5*speed; 
-		babka.y += 0.7*speed; 
+		babka.x -= 2.5*speed*this.acceleration; 
+		babka.y += 0.7*speed*this.acceleration; 
 		
 		if (babka.x < 964) {
 			
-			babka.x -= speed;
-			babka.y -= 0.7*speed;
+			babka.x -= speed*this.acceleration;
+			babka.y -= 0.7*speed*this.acceleration;
 			
 				if (babka.x < 608) {
 					
-					babka.x -= 1.2*speed;
-					babka.y -= 0.2*speed;
+					babka.x -= 1.2*speed*this.acceleration;
+					babka.y -= 0.2*speed*this.acceleration;
 			
 				}
 			
@@ -130,23 +136,23 @@ class LevelOne extends Phaser.Scene {
 	// SZTYWNA SCIEZKA RUCHU BABKI2
 	path2(babka, speed){
 		
-		babka.x -= 2*speed; 
-		babka.y += 1.5*speed;
+		babka.x -= 2*speed*this.acceleration;  
+		babka.y += 1.5*speed*this.acceleration; 
 		
 		if (babka.x < 1095) {
 			
-			babka.x -= 0.5*speed;
-			babka.y -= 1.5*speed;
+			babka.x -= 0.5*speed*this.acceleration; 
+			babka.y -= 1.5*speed*this.acceleration; 
 			
 				if (babka.x < 850) {
 					
-					babka.x -= 1.2*speed;
-					babka.y -= 0.5*speed;
+					babka.x -= 1.2*speed*this.acceleration; 
+					babka.y -= 0.5*speed*this.acceleration; 
 					
 					if (babka.x < 625) {
 						
-						babka.x -= 1*speed;
-						babka.y -= 0.1*speed;
+						babka.x -= 1*speed*this.acceleration; 
+						babka.y -= 0.1*speed*this.acceleration; 
 			
 					}
 					
@@ -157,7 +163,6 @@ class LevelOne extends Phaser.Scene {
 	}
 
  	update() {
-		
 		
 		// UKRYWANIE BABKI1 NA LEWO OD 374PX [KLATKA KOSAKA]
 		if (this.babka1.x < 374) {
@@ -186,15 +191,6 @@ class LevelOne extends Phaser.Scene {
 			}
 			
 		}
-		
-		//if (babkaOdpulona == true) {
-			
-			//this.babka1.setTexture(this.b1);
-			//this.babka1.x = 1170;
-            //this.babka1.y = 490;
-			//this.path2(this.babka1, 2);
-		
-		//}
 		
 	}
 	
