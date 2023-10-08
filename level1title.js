e061a653ba70455cbf79204368144e260f0bf69f
Maciej Kowalczyk <maciejkowalczyk@Maciejs-MacBook-Air.local>
2023-10-08 05:15:00 +0200

    changed font

--- level1title.js
+++ level1title.js
@@ -5,7 +5,7 @@ class Level1Title extends Phaser.Scene {
 	}
 
 	create() {
-		this.gameOverMessage = this.add.text(640,350, 'LEVEL 1: PIASKI', { font: '64px Arial Black Arial-BoldMT', fill: '#00FF00', backgroundColor: '#0' });
+		this.gameOverMessage = this.add.text(640,350, 'LEVEL 1: PIASKI', { font: '64px Verdana Bold', fill: '#00FF00', backgroundColor: '#0' });
 		this.input.on('pointerup', function (pointer) {
         	this.scene.start("level1");
         }, this);
